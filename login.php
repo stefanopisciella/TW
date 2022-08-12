d11ff803264c341d253afe48c62b1d55abe15258
Stefano Pisciella <stefano.pisciella1@student.univaq.it>
2022-08-12 19:39:03 +0200

    aggiunto login.php

---
+++ login.php
@@ -0,0 +1,59 @@
+<?php
+    require "include/template2.inc.php";
+    require "include/tags/utility.inc.php";
+
+    session_start();
+    
+    // una volta loggati correttamente, non è più possibile ritornare alla pagina della login
+    // se non in seguito ad un logout
+    if (isset($_SESSION['user_id']) && $_SESSION['user_id'] != 0) {
+        header('location: index.php');
+    }
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        // caso in cui il client ha già visionato la pagina della login e fa "submit" delle credenziali
+        $username = $_POST['Username'];
+        $password = $_POST['Password'];
+
+        if (!isset($username) 
+                || $username == '' 
+                || !isset($password) 
+                || $password == '') {
+             // credenziali non valide
+            header("Location: login.php?wrong_credentials=1");
+        } else {
+            // cripta la password perché nel DB queste ultime non sono salvate in chiaro
+            $password = md5(md5(md5(md5(md5($password)))));
+            $user_id = login_query($username, $password);
+            if($user_id != 0) {
+                // credenziali corrette
+                $_SESSION['user_id'] = $user_id;
+                header("Location: home.php");
+            } else {
+                // credenziali non corrette
+                header("Location: login.php?wrong_credentials=2");
+            }
+        }
+    } else {
+        // caso in cui il client carica la pagina con il metodo GET
+        if (isset ($_GET['wrong_credentials'])) {
+            $param = $_GET['wrong_credentials'];
+            // client visualizza errore riguardante le credenziali
+            if ($param == 1) {
+                $login = new Template("skins/login.html");
+                $login->setContent("wrong_credentials", "Username e/o Password non sono stanti compilati");
+                $login->close();  
+            }  
+            if ($param == 2) {
+                $login = new Template("skins/login.html");
+                $login->setContent("wrong_credentials", "Username e/o Password non corretti");
+                $login->close();   
+            }
+            session_abort();
+        } else {
+            // caso in cui il client carica la pagina della login, ma non ancora fa ancora il "submit" delle credenziali
+            $login = new Template("skins/login.html");
+            $login->close();  
+        }
+    }
+?>
