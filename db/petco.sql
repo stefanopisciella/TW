6c830bc5278c8975c5f5be3d91c02ae0a0dd3e7c
Nicola Rossi <nicolaxrossi@gmail.com>
2022-08-12 17:08:44 +0200

    db script

---
+++ db/petco.sql
@@ -0,0 +1,193 @@
+# creazione db
+DROP DATABASE IF EXISTS petco;
+CREATE DATABASE IF NOT EXISTS petco;
+
+use petco;
+
+# 12) tabella razza
+create table razza (
+    ID smallint primary key auto_increment,
+    nome varchar(30)
+);
+
+# 13) tabella categoria
+create table categoria (
+    ID smallint primary key auto_increment,
+    nome varchar(50) not null
+);
+
+# 1) tabella cane
+create table cane (
+    ID smallint primary key auto_increment,
+    nome varchar(50) not null,
+    sesso enum('M', 'F') not null,
+    eta smallint not null,
+    razza varchar(50) not null,
+    ID_razza smallint not null,
+    taglia enum('piccola', 'media', 'grande') not null,
+    presentazione text not null,
+    chip char(15) not null,
+    # bit è utilizzato come booleano: se a 1, allora il cane è adottabile a distanza
+    distanza bit not null,
+    adottato bit not null,
+    constraint articolo_razza foreign key (ID_razza) references razza(ID) on update cascade on delete cascade
+);
+
+# 2) tabella utente
+create table utente (
+    ID smallint primary key auto_increment,
+    nickname varchar(25) not null unique,
+    nome varchar(50) not null,
+    cognome varchar(50) not null,
+    passwrd varchar(50) not null,
+    email varchar(100) not null unique,
+    telefono char(10) not null,
+    regione varchar(30) not null,
+    provincia varchar(32) not null,
+    citta varchar(30) not null,
+    via varchar(30) not null,
+    civico tinyint not null
+);
+
+# 3) tabella ugroup
+create table ugroup (
+    ID smallint primary key auto_increment,
+    nome varchar(50),
+    descrizione text
+);
+
+# 4) tabella service
+create table `service` (
+    ID smallint primary key auto_increment,
+    script varchar(200),
+    descrizione text
+);
+
+# 5) tabella articolo
+create table articolo (
+    ID smallint primary key auto_increment,
+    ID_utente smallint,
+    # default 1 indica la categoria 'senza categoria'
+    ID_categoria smallint default 1,
+    titolo varchar(100) not null,
+    contenuto text not null,
+    autore varchar(100) not null,
+    `data` date not null,
+    categoria varchar(30) not null,
+    constraint articolo_utente foreign key (ID_utente) references utente(ID) on update cascade on delete set null,
+    constraint articolo_categoria foreign key (ID_categoria) references categoria(ID) on update cascade on delete set NULL
+    # NOTA: se una categoria viene eliminata, va cambiato il nome della categoria in 'no categoria'
+);
+
+# 6) tabella immagine
+create table immagine (
+    ID smallint primary key auto_increment,
+    ID_cane smallint not null,
+    ID_articolo smallint not null,
+    `path` varchar(200) not null,
+    # indice di visualizzazione dell'immagine
+    indice smallint not null,
+    constraint immagine_cane foreign key (ID_cane) references cane(ID) on update cascade on delete cascade,
+    constraint immagine_articolo foreign key (ID_articolo) references articolo(ID) on update cascade on delete cascade
+);
+
+# 7) tabella richiesta_info
+create table richiesta_info (
+    ID smallint primary key auto_increment,
+    ID_utente smallint not null,
+    `data` date not null,
+    chip char(15) not null,
+    messaggio text not null,
+    constraint info_utente foreign key (ID_utente) references utente(ID) on update cascade on delete cascade
+);
+
+# 8) tabella tag
+create table tag (
+    ID smallint primary key auto_increment,
+    nome varchar(50) not null
+);
+
+# 9) tabella faq
+create table faq (
+    ID smallint primary key auto_increment,
+    domanda varchar(300) not null,
+    risposta text not null
+);
+
+# 10) tabella donazione
+create table donazione (
+    ID smallint primary key auto_increment,
+    importo smallint not null,
+    email varchar(100) not null,
+    `data` date not null
+);
+
+# 11) tabella info_struttura
+create table info_struttura (
+    ID smallint primary key auto_increment,
+    telefono char(10) not null,
+    orario_apertura time not null,
+    orario_chiusura time not null,
+    descrizione text not null
+);
+
+# 14) tabella adozione
+create table richiesta_adozione (
+    ID smallint primary key auto_increment,
+    ID_utente smallint not NULL,
+    ID_cane smallint not null,
+    `data` date not null,
+    documento blob,
+    constraint adozione_utente foreign key (ID_utente) references utente(ID) on update cascade on delete no action,
+    constraint adozione_cane foreign key (ID_cane) references cane(ID) on update cascade on delete no action
+);
+
+# 15) tabella preferiti
+create table preferiti (
+    ID smallint primary key auto_increment,
+    ID_utente smallint not NULL,
+    ID_cane smallint not null,
+    constraint adozione_utente_pref foreign key (ID_utente) references utente(ID) on update cascade on delete no action,
+    constraint adozione_cane_pref foreign key (ID_cane) references cane(ID) on update cascade on delete no action
+
+);
+
+# 16) tabella adozione_distanza
+create table adozione_distanza (
+    ID smallint primary key auto_increment,
+    ID_utente smallint not NULL,
+    ID_cane smallint not null,
+    cadenza tinyint not null,
+    `data` date not null,
+    importo tinyint not null,
+    constraint adozione_utente_dist foreign key (ID_utente) references utente(ID) on update cascade on delete no action,
+    constraint adozione_cane_dist foreign key (ID_cane) references cane(ID) on update cascade on delete no action
+
+);
+
+# 17) tabella user_has_group
+create table user_has_group (
+    ID smallint primary key auto_increment,
+    ID_utente smallint not NULL,
+    ID_gruppo smallint not NULL,
+    constraint user_has_group_utente foreign key (ID_utente) references utente(ID) on update cascade on delete no action,
+    constraint user_has_group_gruppo foreign key (ID_gruppo) references ugroup(ID) on update cascade on delete no action
+);
+
+# 18) tabella ugroup_has_service
+create table ugroup_has_service (
+    ID smallint primary key auto_increment,
+    ID_servizio smallint not NULL,
+    ID_gruppo smallint not NULL,
+    constraint ugroup_has_service_servizio foreign key (ID_servizio) references `service`(ID) on update cascade on delete no action,
+    constraint ugroup_has_service_gruppo foreign key (ID_gruppo) references ugroup(ID) on update cascade on delete no action
+);
+
+# 19) tabella articolo_tag
+create table articolo_tag (
+    ID smallint primary key auto_increment,
+    ID_articolo smallint not null,
+    ID_tag smallint not NULL,
+    constraint articolo_tag_articolo foreign key (ID_articolo) references articolo(ID) on update cascade on delete no action,
+    constraint articolo_tag_tag foreign key (ID_tag) references tag(ID) on update cascade on delete no action
+);
