92eac830baeb25fcb19392216ee31139692d8405
Stefano Pisciella <stefano.pisciella1@student.univaq.it>
2022-08-14 15:07:13 +0200

    aggiunti file di Beatrice

--- admin/home.php
+++ admin/home.php
@@ -3,7 +3,7 @@
 
     session_start();
     if(isset($_SESSION['logged']) && $_SESSION['logged'] != true) {
-        header('location: index.php');
+        header('Location: index.php');
     } else {
         $main = new Template("skins/home.html");
         $main->close();    
