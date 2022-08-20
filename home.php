4ceb15a549bd1f9bbe62ba18ebfc8ef50c078126
Stefano Pisciella <stefano.pisciella1@student.univaq.it>
2022-08-20 12:33:10 +0200

    aggioranto home.php

--- home.php
+++ home.php
@@ -2,8 +2,8 @@
 
     include "include/template2.inc.php";
 
-    $head = new Template("frame-public.html");
-    $item = new Template("home.html");
+    $head = new Template("skins/frame-public.html");
+    $item = new Template("skins/home.html");
 
     $head->setContent("contenuto", $item->get());
     $head->close();
