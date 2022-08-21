6defdcc67221d9b19a32e8804d1e10c870b96561
Stefano Pisciella <stefano.pisciella1@student.univaq.it>
2022-08-21 20:56:17 +0200

    ricerca cane

--- adozioni.php
+++ adozioni.php
@@ -4,8 +4,23 @@
 
     $head = new Template("skins/frame-public.html");
     $adozioni = new Template("skins/adozioni.html");
-
     $head->setContent("contenuto", $adozioni->get());
     
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        // se il client non seleziona alcuna opzione in una select, invierà una stringa vuota
+        // al server
+        $sesso = $_POST['sesso'];
+        $taglia = $_POST['taglia'];
+        $razza = $_POST['razza'];
+        $eta = $_POST['eta'];
+
+        // chiamata al metodo che effettua la ricerca nel DB
+        
+        
+        // REMOVE
+        echo isset($_POST['sesso']);
+        echo $_POST['sesso'] == "";
+    }
+
     $head->close();
 ?>
