d5e29ad6f79f739134283e4b26ba06919a96f20b
Stefano Pisciella <stefano.pisciella1@student.univaq.it>
2022-08-25 13:41:49 +0200

    aggiunta adozione a distanza (testata)

--- dettaglio-cane-distanza.php
+++ dettaglio-cane-distanza.php
@@ -1,29 +1,58 @@
 <?php
     require "include/dbms.inc.php";
     require "frame-public.php";
+    require "include/utils_dbms.php";
 
-    $dettaglio_cane = new Template("skins/dettaglio-cane-a-distanza.html");
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        if(isset($_POST['importo']) && isset($_POST['cadenza'])) {
+            if(!isset($_SESSION['user_id'])) {
+                // caso in cui l'utente non è loggato ==>non può effettuare l'adozione a 
+                // distanza. Pertanto C viene reindirizzato alla login
+                header("Location: login.php");
+                exit;   
+            } else {
+                // caso in cui l'utente è loggato
+                $user_id = $_SESSION['user_id']; 
+                $id_cane = $_POST['id'];
+                $importo = $_POST['importo'];
+                $cadenza = $_POST['cadenza']; 
+                $actual_date = date("Y/m/d");
 
-    // injection informazioni cane
-    $id_cane = $_GET['id'];
-    $query_info_cane = "SELECT nome, presentazione, sesso, eta, razza FROM cane WHERE ID = '{$id_cane}';";
+                $adozione = [$user_id, $id_cane, $cadenza, "'".$actual_date."'", $importo];
 
-    try {
-        $oid = $mysqli->query($query_info_cane);
-    }
-    catch (Exception $e) {
-        throw new Exception("{$mysqli->errno}");
-    }
+                try {
+                    insert_query('adozione_distanza', $adozione);
+                    header("Location: operation-success.php?");
+                } catch (Exception $e){
+                    echo $e;
+                }
+            }
+        
+        }
+    } else {
+        $dettaglio_cane = new Template("skins/dettaglio-cane-a-distanza.html");
 
-    $info_cane = $oid->fetch_all(MYSQLI_ASSOC);
-
-    $dettaglio_cane->setContent("nome", $info_cane[0]["nome"]);
-    $dettaglio_cane->setContent("presentazione", $info_cane[0]["presentazione"]);
-    $dettaglio_cane->setContent("sesso", $info_cane[0]["sesso"]);
-    $dettaglio_cane->setContent("eta", $info_cane[0]["eta"]);
-    $dettaglio_cane->setContent("razza", $info_cane[0]["razza"]);
+        // injection informazioni cane
+        $id_cane = $_GET['id'];
+        $query_info_cane = "SELECT nome, presentazione, sesso, eta, razza FROM cane WHERE ID = '{$id_cane}';";
     
-    $head->setContent("contenuto", $dettaglio_cane->get());
+        try {
+            $oid = $mysqli->query($query_info_cane);
+        }
+        catch (Exception $e) {
+            throw new Exception("{$mysqli->errno}");
+        }
     
-    $head->close();
+        $info_cane = $oid->fetch_all(MYSQLI_ASSOC);
+    
+        $dettaglio_cane->setContent("nome", $info_cane[0]["nome"]);
+        $dettaglio_cane->setContent("presentazione", $info_cane[0]["presentazione"]);
+        $dettaglio_cane->setContent("sesso", $info_cane[0]["sesso"]);
+        $dettaglio_cane->setContent("eta", $info_cane[0]["eta"]);
+        $dettaglio_cane->setContent("razza", $info_cane[0]["razza"]);
+    
+        $head->setContent("contenuto", $dettaglio_cane->get());
+
+        $head->close();
+    }
 ?>
---
+++ operation-success.php
@@ -0,0 +1,3 @@
+<?php 
+    echo "Operazione effettuata con successo";
+?>
