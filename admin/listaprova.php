c72903f3712a5d3801362215eae05ba20e43ee5a
BeatriceTomassi <beatrice.tomassi@student.univaq.it>
2022-08-27 15:07:10 +0200

    due opzioni per liste

---
+++ admin/listaprova.php
@@ -0,0 +1,10 @@
+<?php
+    require "include/template2.inc.php"; 
+    require "include/dbms_ops.php";
+
+    $main = new Template("skins/frame-private.html");
+    $item = new Template("skins/listaprova.html");
+
+    $main->setContent("contenuto", $item->get());
+    $main->close(); 
+?>
