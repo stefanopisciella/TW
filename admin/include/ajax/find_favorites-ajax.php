bbbcb39fc68198f44948f3f713fdde9c65b84f73
Stefano Pisciella <stefano.pisciella1@student.univaq.it>
2022-08-29 11:31:37 +0200

    aggiornamento della cartella include di mazer

---
+++ admin/include/ajax/find_favorites-ajax.php
@@ -0,0 +1,29 @@
+<?php
+    include "../dbms.inc.php";
+    global $mysqli;
+
+    $result = "null";
+
+    session_start();
+
+    if (isset($_SESSION['user_id']))
+    {
+        $id = $_SESSION['user_id'];
+
+        $res = $mysqli->query(
+            "SELECT DISTINCT c.chip FROM utente u 
+                    JOIN preferiti p ON p.ID_utente = $id 
+                    JOIN cane c ON c.ID = p.ID_cane");
+
+        if ($res) {
+            $data = $res->fetch_all(MYSQLI_ASSOC);
+            $i = 0;
+            $result = array();
+            foreach ($data as $row)
+                $result[$i++] = $row['chip'];
+
+            $result = json_encode($result);
+        }
+    }
+
+    echo $result;
