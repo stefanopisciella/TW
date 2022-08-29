c261968bb738c0cf6b8ff62206182d8c6594d6a3
Stefano Pisciella <stefano.pisciella1@student.univaq.it>
2022-08-29 17:05:41 +0200

    m

--- blog.php
+++ blog.php
@@ -5,6 +5,9 @@
 
     // per il redirect allo script "blog" una volta effettuato il login
     $_SESSION['previous_page'] = 'blog';
+    if ($_SERVER["REQUEST_METHOD"] == "GET") {
+        $_SESSION['query_string'] = $_SERVER['QUERY_STRING'];
+    }
 
     global $mysqli;
 
