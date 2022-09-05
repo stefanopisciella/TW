67de4bdf0e6d2e1fe4c60a986092d8cdddac1d3a
stefanopisciella <stefano.pisciella1@student.univaq.it>
2022-09-05 11:11:34 +0200

    db

--- db/petco.sql
+++ db/petco.sql
@@ -239,7 +239,9 @@ INSERT INTO `service`(ID, script, descrizione) VALUES
 	(6, "admin/lista-richieste", "per visualizzare e gestire le richieste di adozione (lato admin)"),
 	(7, "admin/cani-in-struttura", "per visualizzare la tabella dei cani (lato admin)"),
 	(8, "admin/donazioni", "per visualizzare la tabella delle donazioni e delle adozioni a distanza (lato admin)"),
-	(9, "admin/storico-adottati", "per visualizzare la tabella dei cani già adottati (lato admin)");
+	(9, "admin/storico-adottati", "per visualizzare la tabella dei cani già adottati (lato admin)"),
+	(10, "admin/dettaglio-adozione", "per visualizzare i dettagli di una determinata adozione arrivando dalla schermata 'lista-richieste' oppure 'cani-in-struttura' (lato admin)");
+
 
 # popolamento tabella ugroup_has_service 
 INSERT INTO ugroup_has_service(ID, ID_servizio, ID_gruppo) VALUES
@@ -251,7 +253,8 @@ INSERT INTO ugroup_has_service(ID, ID_servizio, ID_gruppo) VALUES
 	(6, 6, 1),
 	(7, 7, 1),
 	(8, 8, 1),
-	(9, 9, 1);
+	(9, 9, 1),
+    (10, 10, 1);
 
 # popolamento tabella categoria
 insert into categoria(tipo, nome) values
