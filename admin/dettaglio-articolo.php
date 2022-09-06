d2bcc776a1e40da26e5760db39f66e1a8079818b
Nicola Rossi <nicolaxrossi@gmail.com>
2022-09-06 15:49:34 +0200

    articolo singolo blog

--- admin/dettaglio-articolo.php
+++ admin/dettaglio-articolo.php
@@ -5,6 +5,59 @@
     $main = new Template("skins/frame-private.html");
     $item = new Template("skins/dettaglio-articolo.html");
 
+    // injection informazioni articolo selezionato
+    $id_articolo = $_GET['art'];
+
+    echo $id_articolo;
+
+    $query = "SELECT titolo, contenuto, categoria, `path` AS img FROM articolo WHERE ID='{$id_articolo}';";
+
+    try {
+        $oid = $mysqli->query($query);
+    }
+    catch (Exception $e) {
+        throw new Exception("{$mysqli->errno}");
+    }
+
+    $info_articolo = $oid->fetch_all(MYSQLI_ASSOC);
+
+    $contenuto = $info_articolo[0]['contenuto'];
+
+    $item->setContent("titolo", $info_articolo[0]['titolo']);
+    $item->setContent("contenuto", $contenuto);
+    $item->setContent("categoria", $info_articolo[0]['categoria']);
+    $item->setContent("img", $info_articolo[0]['img']);
+
+    $query = "SELECT nome FROM tag JOIN articolo_tag ON tag.ID=articolo_tag.ID_tag AND ID_articolo='{$id_articolo}'";
+
+    try {
+        $oid = $mysqli->query($query);
+    }
+    catch (Exception $e) {
+        throw new Exception("{$mysqli->errno}");
+    }
+
+    //$tags = $oid->fetch_all(MYSQLI_ASSOC);
+
+    // costruisco la stringa con i tags separati da virgola
+    $tags = "";
+
+    while($row = mysqli_fetch_array($oid)) {
+        $tags = $tags.$row['nome'].",";
+    }
+
+    $tags = substr($tags, 0, -1);
+
+    $item->setContent("tags", $tags);
+
+    // check che sia richiesta la modifica dell'articolo
+    if (isset($_GET['mod']) && $_GET['mod'] == 1) {
+
+        // si vuole modificare l'articolo
+        // injection contenuto
+        $item->setContent("contenuto", $contenuto);
+    }
+    
     $main->setContent("contenuto", $item->get());
     $main->close(); 
 ?>
