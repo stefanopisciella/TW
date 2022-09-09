2b1699fc1c92eda770f2a3d08fd1641461e98d33
BeatriceTomassi <beatrice.tomassi@student.univaq.it>
2022-09-09 11:49:35 +0200

    cose3

--- admin/aggiungi-adozioni-admin.php
+++ admin/aggiungi-adozioni-admin.php
@@ -300,7 +300,7 @@
 
         $imgs_path = upload_images();
 
-        $cane = ["'".$nome."'", "'".$sesso."'", "'".$eta."'", "'".$razza."'", "'".$taglia."'", "'".$descrizione."'", "'".$chip."'", $a_distanza, false];
+        $cane = ["'".$nome."'", "'".$sesso."'", "'".$eta."'", "'".$razza."'", "'".$taglia."'", "'".$descrizione."'", "'".$chip."'", $a_distanza, 0];
         
         try {
             if(isset($imgs_path)) {
